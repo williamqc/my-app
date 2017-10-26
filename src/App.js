966b58e0c1be24fb40bd4afe7f708591fe8f6c9b
UberAV <cronosstomun@gmail.com>
2017-10-26 12:54:35 -0500

    2

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+//cuando tenemos dos parametros  nesesitamos parentesis si es uno no
+   handleClick = e => {
+    e.preventDefault();
+    console.log('The link was clicked.');
+   }
   render() {
+    //este es igual al html pero  no es, es un jsx de facebook
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">XXWelcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+
+          <a href="#" onClick={this.handleClick}>
+      Click me
+    </a>
         </p>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+//archivo de arranque  es como el main
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
